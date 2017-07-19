bb6e33d304fe34d30c92ecd90196e84056bbdf8a
traviswight1 <traviswight@Traviss-MacBook-Pro.local>
2017-07-18 20:45:10 -0700

    I'm back... and I've gotten a lot done (7/18/17)
    I’ve got a basic design. I’m working with the black bar design with its
    color scheme. I set up the parallax parts, so all I have to do is the
    rest of the page. I put a “coming soon” animation on the incomplete
    parts and finished the contact me section. This update is live.

--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
 //javascript practice
 var string = "banana, phone, time, warp, destroy, build, apple, matt, damon, robin, williams, star, wars"
 console.log();
+var time = new Date();
+document.getElementById('test').innerHTML = time.getMonth() + "/" + time.getDate() + "/" + time.getFullYear();
